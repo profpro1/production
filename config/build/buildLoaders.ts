a56ec0e136235898c5793ddf53e823e3b9dcef25
pfopro1 <borduzenkoigor@gmail.com>
2023-05-16 13:43:09 +0300

    add babel

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -4,7 +4,25 @@ import { BuildOptions } from './types/config';
 
 
 export function buildLoaders({isDev}:BuildOptions):webpack.RuleSetRule[] {
-
+    const baberLoader = {
+      test: /\.(js|jsx|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env'],
+            "plugins":[
+              [
+                "i18next-extract",
+                {
+                  locales:['ru','en'],
+                  keyAsDefaultValue:true,
+                }
+              ],
+            ]
+          }
+        }
+    }
     const cssLoader = {
         test: /\.s[ac]ss$/i,
         use: [
@@ -49,6 +67,7 @@ export function buildLoaders({isDev}:BuildOptions):webpack.RuleSetRule[] {
     return  [
         fileLoader,
         svgLoader,
+        baberLoader,
         typescriptLoader,
         cssLoader,
     ]
