866dffd8a2169c03404ee42a354ddd61a3985d64
pfopro1 <borduzenkoigor@gmail.com>
2023-05-17 00:41:30 +0300

    add jest ,sample jest

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,36 @@
+import { classNames } from "./classNames";
+
+
+describe('classNames',()=> {
+    test('with only first param',()=> {
+        expect(classNames('someClass')).toBe('someClass');
+    });
+    
+    test('with additional class',()=> {
+        const expected = 'someClass class1 class2';
+        expect(classNames('someClass',
+        {},
+        ['class1','class2'])).toBe(expected);
+    });
+    
+    test('with mods',()=> {
+        const expected = 'someClass class1 class2 hovered';
+        expect(classNames('someClass',
+        {hovered:true},
+        ['class1','class2'])).toBe(expected);
+    });
+    
+    test('with mods false',()=> {
+        const expected = 'someClass class1 class2 scrollable';
+        expect(classNames('someClass',
+        {hovered:false,scrollable:true},
+        ['class1','class2'])).toBe(expected);
+    });
+    
+    test('with mods undefined',()=> {
+        const expected = 'someClass class1 class2';
+        expect(classNames('someClass',
+        {hovered:false,scrollable:undefined},
+        ['class1','class2'])).toBe(expected);
+    });
+})
